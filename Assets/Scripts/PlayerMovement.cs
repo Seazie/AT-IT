5f2629bded930dc6d41731f3b95b1c698969da29
Seazie <carlitoszaza@gmail.com>
2023-05-03 23:08:59 -0500

    bleh!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     float hMove = 0f;
     bool jump = false;
     public float runSpeed = 40f;
-    [Header ("Attack Stuff")]
+    [Header("Attack Stuff")]
     public float attackDistance = 1.0f;
     public int damage = 10;
     public float attackCooldown = 1.0f;
@@ -18,64 +18,64 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       hMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-       animator.SetFloat("Speed", Mathf.Abs(hMove));
+        hMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(hMove));
 
-       if (Input.GetKeyDown(KeyCode.W))
-       {
-            jump = true;
-       }
-
-       {
-        // Check if enough time has passed since last attack
-        if (Time.time - lastAttackTime > attackCooldown)
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            // Check for space bar tap
-            if (Input.GetKeyDown(KeyCode.Space))
+            jump = true;
+        }
+
+        {
+            // Check if enough time has passed since last attack
+            if (Time.time - lastAttackTime > attackCooldown)
             {
-                animator.SetBool("Attack", true);
-                // Set last attack time to current time
-                lastAttackTime = Time.time;
-
-                
-                // Raycast to find the enemy
-                Ray ray = new Ray(transform.position, transform.right);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit, attackDistance))
+                // Check for space bar tap
+                if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    // Check if the object hit is an enemy
-                    if (hit.collider.CompareTag("Enemy"))
+                    animator.SetBool("Attack", true);
+                    // Set last attack time to current time
+                    lastAttackTime = Time.time;
+
+
+                    // Raycast to find the enemy
+                    Ray ray = new Ray(transform.position, transform.right);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit, attackDistance))
                     {
-                        // Calculate direction of attack based on A/D key input
-                        Vector3 direction = Vector3.zero;
-                        if (Input.GetKey(KeyCode.A))
+                        // Check if the object hit is an enemy
+                        if (hit.collider.CompareTag("Enemy"))
                         {
-                            direction = -transform.right;
-                        }
-                        else if (Input.GetKey(KeyCode.D))
-                        {
-                            direction = transform.right;
-                        }
+                            // Calculate direction of attack based on A/D key input
+                            Vector3 direction = Vector3.zero;
+                            if (Input.GetKey(KeyCode.A))
+                            {
+                                direction = -transform.right;
+                            }
+                            else if (Input.GetKey(KeyCode.D))
+                            {
+                                direction = transform.right;
+                            }
 
-                        // Calculate position of attack based on direction and attack distance
-                        Vector3 attackPosition = transform.position + direction * attackDistance;
+                            // Calculate position of attack based on direction and attack distance
+                            Vector3 attackPosition = transform.position + direction * attackDistance;
 
-                        // Calculate damage to apply to enemy
-                        Enemy enemy = hit.collider.GetComponent<Enemy>();
-                        if (enemy != null)
-                        {
-                            enemy.TakeDamage(damage);
+                            // Calculate damage to apply to enemy
+                            Enemy enemy = hit.collider.GetComponent<Enemy>();
+                            if (enemy != null)
+                            {
+                                enemy.TakeDamage(damage);
+                            }
+
                         }
-
                     }
                 }
             }
         }
     }
-    }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         controller.Move(hMove * Time.fixedDeltaTime, jump);
         jump = false;
